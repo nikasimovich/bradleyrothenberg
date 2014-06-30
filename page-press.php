f03433ffc9c13b8834e380d5ab19acc090fa8f1f
Nika SImovich <nika@nikasimovich.com>
2014-06-29 23:50:56 -0400

    Press page update

--- page-press.php
+++ page-press.php
@@ -8,6 +8,9 @@ Template Name: Page - Press
             <div id="page-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
                 <!--<h2 class="entry-title"><?php the_title() ?></h2>!-->
                 <div class="entry-content">
+                  <div class="press-contact">
+                      For press inquiries please email <a href="mailto:press@brandleyrothenberg.com?Subject=Hello" target="_top">press@brandleyrothenberg.com.</a></span>
+                  </div>
 
                   <?php
                     // Arguments
@@ -27,7 +30,7 @@ Template Name: Page - Press
 
                   <div class="press">
                     <h4>
-                      <a href="<?php echo $press_url ?>"><?php the_content() ?></a>
+                      <a href="<?php echo $press_url ?>" target="_blank"><?php the_content() ?></a>
                     </h4>
                   </div>
 
