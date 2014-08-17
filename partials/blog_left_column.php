de22f9c3bb7fc3efab1c2bd8253ca84aec8524e0
Nika SImovich <nika@nikasimovich.com>
2014-08-17 17:58:31 -0400

    changed mailing list in research page
    changed mailing list in research page

--- partials/blog_left_column.php
+++ partials/blog_left_column.php
@@ -1,16 +1,26 @@
 <div id="left-column">
   <div class="blog-info">
     <span class="blog-info-title">Subscribe</span>
-    <?php
-      $twitter = get_field('twitter', 19);
-      $researchPost = get_post(19);
-      echo $researchPost->post_content;
-    ?>
+      <div id="blog-mailing">
+              <div id="mc_embed_signup" class="blogmailing">
+              <form class="form" action="http://bradleyrothenberg.us8.list-manage.com/subscribe/post?u=a2dee844d5f0278bf010e6bd0&amp;id=2e07a9a8cb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <!-- <label for="mce-EMAIL">Sign up for our mailing list</label> -->
+                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="E-mail sign up" required>
+                  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                  <div style="position: absolute; left: -5000px;"><input type="text" name="b_a2dee844d5f0278bf010e6bd0_2e07a9a8cb" tabindex="-1" value=""></div>
+                  <div  class="clear form"><input type="submit" value="Ok" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+              </form>
+              </div>
+      </div>
+
   </div>
 
   <div class="blog-info">
     <span class="blog-info-title">Tweet us</span>
-   <?php echo $twitter; ?>
+   <?php
+   $twitter = get_field('twitter', 19);
+   $researchPost = get_post(19);
+   echo $twitter; ?>
   </div>
 
   <div class="blog-info">
